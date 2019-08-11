46f45d422c122420b6cd2363d963cdbe8225b488
Fil Bryan <filbryancegos@gmail.com>
2019-08-11 21:24:11 +0800

    real world vue event listing

---
+++ src/views/EventList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+   <h1>Event Listing</h1>
+  
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+   
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
+
+
+export default {
+  name: "EventList",
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    EventService.getEvents()
+    .then(response => {
+      this.events = response.data
+    })
+    .catch(error => console.log(error))
+
+    
+  }
+};
+</script>
